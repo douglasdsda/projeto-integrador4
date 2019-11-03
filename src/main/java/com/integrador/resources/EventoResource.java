822b2f5a25de93c298e96a8c5c06ef150af879f1
Douglas S Anjos <douglasdsda@gmail.com>
2019-11-03 20:04:40 -0300

    evento update

--- src/main/java/com/integrador/resources/EventoResource.java
+++ src/main/java/com/integrador/resources/EventoResource.java
@@ -2,11 +2,16 @@ package com.integrador.resources;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +26,6 @@ public class EventoResource {
 	private EventoService service;
 
 	@GetMapping
-	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<List<EventoDTO>> findAll() {
 		List<EventoDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
@@ -30,6 +34,19 @@ public class EventoResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<EventoDTO> findById(@PathVariable Integer id) {
 		EventoDTO obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventoDTO> update(@PathVariable Integer id, @Valid @RequestBody EventoDTO obj) {
+		service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 
--- src/main/java/com/integrador/services/EventoService.java
+++ src/main/java/com/integrador/services/EventoService.java
@@ -3,14 +3,18 @@ package com.integrador.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.integrador.dto.EventoDTO;
 import com.integrador.entities.Evento;
-import com.integrador.repository.CategoriaRepository;
 import com.integrador.repository.EventoRepository;
-import com.integrador.repository.UsuarioRepository;
+import com.integrador.services.exceptions.DatabaseException;
 import com.integrador.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -18,12 +22,6 @@ public class EventoService   {
 
 	@Autowired
 	private EventoRepository repository;
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private UsuarioRepository usuarioRepository;
 	
 	public List<EventoDTO> findAll() {
 		List<Evento> list = repository.findAll();
@@ -33,6 +31,35 @@ public class EventoService   {
 	public EventoDTO findById(Integer id) {
 		Evento entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
 		return new EventoDTO(entity);
+	}
+
+	public void delete(Integer id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	@Transactional
+	public EventoDTO update(Integer id, EventoDTO obj) {
+		try {
+			Evento entity = repository.getOne(id);
+			updateData(entity, obj);
+			entity = repository.save(entity);
+			return new EventoDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);			
+		}
+	}
+	
+	private void updateData(Evento entity, EventoDTO obj) {
+		entity.setData(obj.getData());
+		entity.setDescricao(obj.getDescricao());
+		entity.setLocalNome(obj.getLocalNome());
+		entity.setTitulo(obj.getTitulo());
 	}
 	
 	
